7f6dc628a27fbaa45df07b4a23cfaa677e6fbd7b
JacekWit <jacek_witkowski@interia.pl>
2017-07-08 09:09:15 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-!       
+$(function () {
+    $(window).scroll(function () {
+    if ($(document.scrollTop() >= 50) {
+        $('#main-nav').addClass('scroll');
+        $('#main-nav').css({
+        'transition': "all 0.3s"
+        });
+        } else {
+    $('#main-nav').removeClass('scroll');
+
+            }
+    });
+if ($(document.scrollTop() >= 50) {
+$('#main-nav').addClass('scroll');
+
+ });
